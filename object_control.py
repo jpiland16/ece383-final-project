c8273497835989dc7e4d4a3ef86499af365e03b0
Jonathan Piland <jonathan.piland@duke.edu>
2023-11-22 21:05:46 -0500

    demonstrate spawning capabilities

--- object_control.py
+++ object_control.py
@@ -94,17 +94,60 @@ class ObjectController:
     def get_model_names(self):
         return self._get_properties().model_names
 
-if __name__ == "__main__":
-    pass
+def set_up_staged_tokens(oc: ObjectController):
+    # create 7-by-3 array of tokens and holders (so they stay still)
+    for i in range(7): # x index
+        for j in range(3): # y index
+            x = 0.6 + 0.05 * i
+            y = 0.0 + 0.05 * j
+            oc.spawn_holder_at_location(x, y)
+            oc.spawn_token_at_location(TokenColor.RED, x, y + 0.007, 0.05)
 
-oc = ObjectController()
-oc.delete_all_free_models()
-# res = oc.delete_model_by_name("holder10")
+def drop_token_in_column(oc: ObjectController, color: TokenColor, column: int):
+    if (not (1 <= column <= 7)) or (int(column) != column):
+        raise ValueError(f"Invalid column id: {column}")
+    
+    # x_pos = [0.518, 0.551, 0.585, 0.620, 0.655, 0.690, 0.725][column - 1]
+    x_pos = 0.518 + 0.0345 * (column - 1)
+
+    oc.spawn_token_at_location(color, x_pos, -0.548, 0.5)
+
+def main():
+    oc = ObjectController()
+    oc.delete_all_free_models()
+    set_up_staged_tokens(oc)
+    drop_token_in_column(oc, TokenColor.RED, 1)
+    drop_token_in_column(oc, TokenColor.YELLOW, 2)
+    drop_token_in_column(oc, TokenColor.RED, 3)
+    drop_token_in_column(oc, TokenColor.YELLOW, 4)
+    drop_token_in_column(oc, TokenColor.RED, 5)
+    drop_token_in_column(oc, TokenColor.YELLOW, 6)
+    drop_token_in_column(oc, TokenColor.RED, 7)
+    counter = 1
+    players = [TokenColor.RED, TokenColor.YELLOW]
+    while True:
+        col = input("Enter column (1 - 7) to play (-1 to exit) >>> ")
+        try:
+            col = int(col)
+            if col == -1:
+                break
+            drop_token_in_column(oc, players[counter % 2], col)
+            counter += 1
+        except:
+            print("ERROR!")
+            pass
+
+if __name__ == "__main__":
+    main()
+
+# oc = ObjectController()
+# oc.delete_all_free_models()
+# # res = oc.delete_model_by_name("holder10")
+# # print(res)
+# res = oc.spawn_holder_at_location(0.70, 0)
 # print(res)
-res = oc.spawn_holder_at_location(0.70, 0)
-print(res)
-# res = oc.delete_model_by_name("token10")
+# # res = oc.delete_model_by_name("token10")
+# # print(res)
+# res = oc.spawn_token_at_location(TokenColor.RED, 0.7, 0.007, 0.05)
 # print(res)
-res = oc.spawn_token_at_location(TokenColor.RED, 0.7, 0.007, 0.05)
-print(res)
 
